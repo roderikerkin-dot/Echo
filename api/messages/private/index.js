25d83135ab1a74a2f12a8e13a5ff20b14fd258d1
roderikerkin-dot <roderikerkin@gmail.com>
2026-01-12 01:19:52 +0300

    Fix API routes to use centralized server file

---
+++ api/messages/private/index.js
@@ -0,0 +1,16 @@
+// Импортируем приложение из нашего серверного файла
+const app = require('../../../vercel-server-supabase-clean.js');
+
+// Создаем обработчик для Vercel
+module.exports = async (req, res) => {
+  // Передаем управление нашему приложению Express
+  await new Promise((resolve, reject) => {
+    app(req, res, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+};
